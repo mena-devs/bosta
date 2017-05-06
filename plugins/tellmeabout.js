1b37207b32dc65129eedc912dec9c3872b87ded7
Omar Abou Mrad <omar.aboumrad@gmail.com>
2017-05-06 12:48:57 +0300

    tellmeabout resorts to ddg search if not found

--- plugins/tellmeabout.js
+++ plugins/tellmeabout.js
@@ -1,17 +1,68 @@
+const https = require('https');
+
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 const storage = require('node-persist');
 
 const META = {
     name: 'tellmeabout',
-    short: 'records and plays text',
+    short: 'records, plays or searchs the interwebs',
     examples: [
         '@bosta save snippet-security-risks as: the snippets are run in a container, they are perfectly sandboxed.',
         '@bosta about snippet-security-risks',
         '@bosta save stop-writing-classes as: https://www.youtube.com/watch?v=o9pEzgHorH0',
         '@bosta about stop-writing-classes',
+        '@bosta forget key-to-delete',
     ],
 };
+
+const API = '/?format=json&q=';
+
+function cleanup(text) {
+    const pattern = /<a.*>(.*)<\/a>\s*(.*)/;
+    const [, key, value] = text.match(pattern) || [];
+
+    return `${key}: ${value}`;
+}
+
+function search(term) {
+    const options = {
+        host: 'duckduckgo.com',
+        path: API + encodeURIComponent(term),
+    };
+
+    return new Promise((resolve, reject) => {
+        https.get(options, (res) => {
+            let body = '';
+
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            res.on('end', () => {
+                const json = JSON.parse(body);
+
+                if (json) {
+                    const topics = json.RelatedTopics
+                        .filter(topic => topic.Result !== undefined)
+                        .map(topic => cleanup(topic.Result))
+                        .join('\n');
+
+                    if (topics) {
+                        resolve(topics);
+                    } else {
+                        resolve(`I don't know anything about ${term}`);
+                    }
+                } else {
+                    resolve('invalid response, sorry!');
+                }
+            });
+        }).on('error', (error) => {
+            reject(error);
+        });
+    });
+}
+
 
 function register(bot, rtm, web, config) {
     storage.init({
@@ -41,9 +92,25 @@ function register(bot, rtm, web, config) {
                     .getItem(key)
                     .then((value) => {
                         if (value) {
-                            rtm.sendMessage(
-                                `${key}: ${value}`,
-                                message.channel);
+                            return `${key}: ${value}`;
+                        }
+
+                        return search(key);
+                    })
+                    .then(value => rtm.sendMessage(`${value}`, message.channel))
+            }
+        }
+
+        if (message.text) {
+            const pattern = /<@([^>]+)>:? forget (.*)/;
+            const [, target, key] = message.text.match(pattern) || [];
+
+            if (target === bot.self.id) {
+                storage
+                    .removeItem(key)
+                    .then((value) => {
+                        if (value) {
+                            rtm.sendMessage(`${key} removed.`, message.channel);
                         } else {
                             rtm.sendMessage(
                                 `I do not know anything about ${key}`,
