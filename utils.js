2e92f9e5d9068ec5b68057335a9c56d3cc8d93d6
Bassem Dghaidi <link-@github.com>
2020-07-03 21:22:15 +0200

    Fix message.reply to use web api instead of RTM

--- utils.js
+++ utils.js
@@ -2,16 +2,23 @@ function pre (text) {
   return `\`\`\`${text}\`\`\``
 }
 
-function patch (rtm, web, message) {
-  message.reply = (text) => rtm.sendMessage(text, message.channel)
+function patch (web, message) {
+  // Reply in channel or in DM
+  message.reply = (text) => web.chat.postMessage({
+    as_user: true,
+    channel: message.channel,
+    text
+  })
+  // Reply to the user's message in a thread
   message.reply_thread = (text) => {
     web.chat.postMessage({
       channel: message.channel,
       text: text,
       as_user: true,
       thread_ts: message.ts
-    }).catch((error) => console.error(error))
+    })
   }
+  // Reply using the blocks API
   message.reply_blocks = (text, blocks) => {
     web.chat.postMessage({
       channel: message.channel,
@@ -19,8 +26,9 @@ function patch (rtm, web, message) {
       blocks: blocks,
       as_user: true,
       thread_ts: message.ts
-    }).catch((error) => console.error(error))
+    })
   }
+  // Reply with an emoji
   message.react = (emoji) => {
     web.reactions.add({
       channel: message.channel,
